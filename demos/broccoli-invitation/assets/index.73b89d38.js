e8eb0ed2fc2d832d86211eff05ffdfdf41b46c27
youknowznm <scarletaeonia@163.com>
2022-04-06 02:50:47 +0800

    add airwallex test

---
+++ demos/broccoli-invitation/assets/index.73b89d38.js
@@ -0,0 +1,1 @@
+var o={siteName:"Broccoli & Co.",authorGithub:"https://github.com/youknowznm",authorName:"youknowznm"};export{o as c};
---
+++ demos/broccoli-invitation/assets/index.e62c896b.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.79d975b3.js";import{c as e}from"./classnames.956b8db4.js";var t=r=>a("div",{className:e("material-container","material-responsive"),children:r.children});export{t as default};
