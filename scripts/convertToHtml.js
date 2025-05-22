da13a960a2cf15ecea01914db2fc6b8d46eb7c6f
youknowznm <scarletaeonia@163.com>
2025-05-22 10:55:46 +0800

    update structure

--- scripts/convertToHtml.js
+++ scripts/convertToHtml.js
@@ -13,19 +13,32 @@ const getPageHtml = (blogName, blogHtml) => `
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="stylesheet" href="../styles/global.css">
       <link rel="icon" href="../styles/avatar.png">
-      <link rel="stylesheet" href="../utils/highlightjs/default.min.css">
+      <link rel="stylesheet" href="../utils/highlightjs/catppuccin-macchiato.css" media="(prefers-color-scheme: dark)">
+      <link rel="stylesheet" href="../utils/highlightjs/catppuccin-latte.css" media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)">
       <script src="../utils/highlightjs/highlight.min.js"></script>
       <script>hljs.highlightAll()</script>
     </head>
     <body>
-      <div class="blog-content markdown-body">
-        ${blogHtml}
+      <div class="container">
+        <div class="blog-nav">
+          <ul>
+            <li><a href="">faflfja;dlfjad;lfjdlfja;fjaldfj;daf;s</a></li>
+            <li><a href="">faflfj</a></li>
+            <li><a href="">faflfja;dlfjad;lfjdlfja;fjaldfj;daf;s</a></li>
+          </ul>
+        </div>
+<!--        <div class="nav-trigger" />-->
+        <div class="blog-content-wrap">
+          <div class="blog-content markdown-body">
+            ${blogHtml}
+          </div>
+        </div>
+        <footer class="blog-footer">
+          <p class="blog-footer-code">
+            Made with ❤️ by <a href="${githubLink}" target="_blank" class="blog-footer-code">youknowznm</a>.
+          </p>
+        </footer>
       </div>
-      <footer class="blog-footer">
-        <p class="blog-footer-code">
-          Made with ❤️ by <a href="${githubLink}" target="_blank" class="blog-footer-code">youknowznm</a>.
-        </p>
-      </footer>
     </body>
   </html>
 `
@@ -34,6 +47,13 @@ const blogSourceDir = './blogSource'
 const blogHtmlDir = './blogs'
 
 const blogSourceNameList = readdirSync(blogSourceDir)
+
+const renderer = new marked.Renderer()
+renderer.link = ({href, text}) => `<a target="_blank" href="${href}">${text}` + '</a>'
+marked.setOptions({
+  renderer,
+  gfm: true
+})
 
 blogSourceNameList.forEach(nameWithExt => {
   if (!nameWithExt.endsWith('.md')) {
